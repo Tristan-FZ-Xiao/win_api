6fe41fb89b55ca456e9374fa352b27c0fe4c9c2c
tristan xiao <tristan_xiao@qq.com>
2016-11-04 00:26:00 +0800

    add role info: HP, MP, level and gold.

--- recognise.cpp
+++ recognise.cpp
@@ -9,12 +9,28 @@
 #include "snap_screen.h"
 #include "recognise.h"
 
+struct hp_info {
+	int cur_hp;
+	int hp;
+};
+
+struct mp_info {
+	int cur_mp;
+	int mp;
+};
+
+struct role_status {
+	struct hp_info hp;
+	struct mp_info mp;
+	int gold;
+	int level;
+} role_info;
 /*	What we need in the module: do some basic recognition likes:
  *	1. Number;
  */
 /*	The charactor of 0 - 9
  */
-int num_charactor[10] = {
+int num_charactor[11] = {
 	62226,	/* 0 */
 	281,	/* 1 */
 	43333,	/* 2 */
@@ -24,7 +40,22 @@ int num_charactor[10] = {
 	53334,	/* 6 */
 	1532,	/* 7 */
 	53335,	/* 8 */
-	43335	/* 9 */	
+	43335,	/* 9 */
+	2222	/* / */
+};
+
+enum {
+	NUM_0 = 0,
+	NUM_1,
+	NUM_2,
+	NUM_3,
+	NUM_4,
+	NUM_5,
+	NUM_6,
+	NUM_7,
+	NUM_8,
+	NUM_9,
+	NUM_M,	/* / */
 };
 
 int get_num_digit(int num)
@@ -73,7 +104,7 @@ int get_num_charactor(int num)
 {
 	int i = 0;
 
-	for (; i < 10; i ++) {
+	for (; i < sizeof(num_charactor); i ++) {
 		if (num_charactor[i] == num) {
 			return i;
 		}
@@ -87,7 +118,10 @@ char *__recognise_image_num(char *ptr, int *len, int *out_num)
 	int num = 0;
 	int target = 0;
 
-	for (i = 0; i < 5; i ++) {
+	for (i = 0; i < 15; i ++) {
+		if (0 == *(ptr + i)) {
+			break;
+		}
 		num = get_num(num, *(ptr + i));
 		target = get_num_charactor(num);
 		if (-1 == target) {
@@ -140,49 +174,98 @@ void print_charactor(unsigned char ptr[][5], int len)
 	}
 }
 
-void get_role_HP(void)
+int get_role_item(struct t_bmp *input, RECT target_rc, int *info)
 {
-	struct t_bmp input = {};
 	struct t_bmp output = {};
-	int ret = 0;
-	RECT target_rc = {};
-	unsigned char nums_charactor[1000] = {};
-
-	/* Get the role's HP/ position*/
-	target_rc.left = 170;
-	target_rc.right = 250;
-	target_rc.top = 283;
-	target_rc.bottom = 291;
-
-	ret = load_picture(L"D://role_m.bmp", &input);
-	if (ret != ERR_NO_ERR) {
-		return;
-	}
-
-	ret = get_screen_rect(&input, target_rc, &output);
-	if (ret != ERR_NO_ERR) {
-		delete[] input.data;
-		return;
-	}
-	ret = convert_gray(&output, BINARY_WEIGHTED_MEAN);
-	ret = convert2blackwhite(&output, ONLY_BLACK);
-	calc_number_charactor(output.data, output.len, nums_charactor);
-
+	unsigned char *nums_charactor = NULL;
 	char *ptr = NULL;
-	int recognise_num = 0;
-	int len = output.len / (8 * 4);
-	ptr = (char *)nums_charactor;
+	int ret = 0;
+	int len = 0;
+	int tmp = 0;
+	int i;
 
+	ret = get_screen_rect(input, target_rc, &output);
+	if (ret != ERR_NO_ERR) {
+		return ret;
+	}
+	//save_picture(L"D://tmp.bmp", &output);
+	len = output.len / ((target_rc.bottom - target_rc.top) * 4);
+	nums_charactor = new unsigned char[len];
+	memset(nums_charactor, 0 , len);
+	calc_number_charactor(output.data, output.len, nums_charactor);
+	ptr = (char *)nums_charactor;
+#if 0
+	for (i = 0; i < len; i ++) {
+		printf("%d ", *(ptr + i));
+	}
+	printf("\n");
+#endif
+	i = 0;
 	do {
-		ptr = recognise_image_num(ptr, &len, &recognise_num);
-		if (recognise_num == -1) {
+		ptr = recognise_image_num(ptr, &len, &tmp);
+		if (tmp == -1) {
 			continue;
 		}
-		printf("recognise number: %d\n", recognise_num);
+
+		if (NUM_M == tmp) {
+			i ++;
+		}
+		else {
+			info[i] = info[i] * 10 + tmp;
+		}
+		//printf("recognise number: %d\n", tmp);
 	} while (len > 0 && *ptr != ' ');
 
-	delete[] input.data;
+	delete[] nums_charactor;
 	delete[] output.data;
+	return ERR_NO_ERR; 
+}
+
+int get_role_hp_mp_level(struct t_bmp *input, struct role_status *role)
+{
+	RECT rc;
+	int ret = 0;
+
+	/* Get the role's HP position*/
+	rc.left = 160;
+	rc.right = 250;
+	rc.top = 283;
+	rc.bottom = 291;
+	ret = get_role_item(input, rc, (int *)&role->hp);
+	printf("Current HP %d, HP %d\n", role->hp.cur_hp, role->hp.hp);
+
+	/* Get the role's MP position*/
+	rc.left = 290;
+	rc.right = 375;
+	rc.top = 283;
+	rc.bottom = 291;
+	ret = get_role_item(input, rc, (int *)&role->mp);
+	printf("Current MP %d, MP %d\n", role->mp.cur_mp, role->mp.mp);
+
+	/* Get the role's level position*/
+	rc.left = 200;
+	rc.right = 242;
+	rc.top = 243;
+	rc.bottom = 251;
+	ret = get_role_item(input, rc, (int *)&role->level);
+	printf("Level %d\n", role->level);
+	return ret;
+}
+
+int get_role_gold(struct t_bmp *input, struct role_status *role)
+{
+	RECT rc;
+	int ret = 0;
+
+	/* Get the role's Gold position*/
+	rc.left = 620;
+	rc.right = 690;
+	rc.top = 535;
+	rc.bottom = 543;
+	ret = get_role_item(input, rc, (int *)&role->gold);
+	printf("Current gold %d\n", role->gold);
+
+	return ret;
 }
 
 void unit_test_show_nums_charactor(void)
@@ -254,7 +337,33 @@ int main()
 	//unit_test_get_num_charctor_by_num();
 	//unit_test_calc_image_charactor();
 	//unit_test_recognise_image_num();
-	get_role_HP();
+	struct t_bmp input = {};
+	int ret = 0;
+
+	ret = load_picture(L"D://role_m.bmp", &input);
+	if (ret != ERR_NO_ERR) {
+		return 0;
+	}
+	ret = convert_gray(&input, BINARY_WEIGHTED_MEAN);
+	ret = convert2blackwhite(&input, ONLY_BLACK);
+	ret = get_role_hp_mp_level(&input, &role_info);
+	delete input.data;
+
+	ret = load_picture(L"D://role_i.bmp", &input);
+	if (ret != ERR_NO_ERR) {
+		return 0;
+	}
+
+	ret = convert_gray(&input, BINARY_WEIGHTED_MEAN);
+	ret = convert2blackwhite(&input, ONLY_BLACK);
+	ret = get_role_gold(&input, &role_info);
+
+	if (ret != ERR_NO_ERR) {
+		return ret;
+	}
+
+	//save_picture(L"D://tmp_role_m.bmp", &input);
+	delete input.data;
 
 	return 0;
 }
